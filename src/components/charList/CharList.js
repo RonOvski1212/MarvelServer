2aeb20dda07c6e455d5b07c35eb81c94b53c10ac
RonOvski1212 <245785gio@gmail.com>
2023-10-09 23:36:19 +0400

    added comic page and debug erors page

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -9,7 +9,7 @@ import "./charList.scss";
 const CharList = (props) => {
   const [charList, setCharList] = useState([]);
   const [newItemLoading, setNewItemLoading] = useState(false);
-  const [offset, setOffset] = useState(210);
+  const [offset, setOffset] = useState(0);
   const [charEnded, setCharEnded] = useState(false);
 
   const { loading, error, getAllCharacters } = useMarvelService();
---
+++ src/components/pages/404.js
@@ -0,0 +1,35 @@
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import { Link } from "react-router-dom";
+
+const Page404 = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: "block",
+          textAlign: "center",
+        }}
+      >
+        <ErrorMessage />
+      </div>
+
+      <p style={{ textAlign: "center", fontWeight: "bold", fontSize: "16px" }}>
+        Page doesn't exist
+      </p>
+      <Link
+        style={{
+          display: "block",
+          textAlign: "center",
+          fontWeight: "bold",
+          fontSize: "24px",
+          marginTop: "30px",
+        }}
+        to="/"
+      >
+        Back to main page
+      </Link>
+    </div>
+  );
+};
+
+export default Page404;
